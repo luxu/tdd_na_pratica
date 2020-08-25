6b64fb6fcbb1e24e79b7d504d2119e9617316d44
Luciano Martins <zicadopv@gmail.com>
2020-08-25 15:03:32 -0300

    Primeiro FT especifico nos comentarios, utilizando agora o unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Edith ouviu falar de uma nova aplicação online interessante para
-# lista de tarefas. Ela decide verificar sua homepage
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-# Ela percebe que o título da página e o cabeçalho mencionam listas de
-# tarefas (to-do)
-assert 'To-Do' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
 
-# Ela é convidada a inserir um item de tarefa imediatamente
+        # Ela percebe que o título da página e o cabeçalho mencionam listas de
+        # tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
-# de texto (o hobby de Edith é fazer iscas para pesca com fly)
+        # Ela é convidada a inserir um item de tarefa imediatamente
+
+        # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
+        # de texto (o hobby de Edith é fazer iscas para pesca com fly)
+
+        # Quando ela tecla enter, a página é atualizada, e agora a página lista
+        # "1: Buy peacock feathers" como um item em uma lista de tarefas
+
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
+        # item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
+        # para fazer um fly - Edith é bem metódica)
+
+        # A página é atualizada novamente e agora mlstra os dois itens em sua lista
+
+        # Edith se pergunta se o site lembrará de sua lista. Então ele nota
+        # que o site gerou uma URL único para ela -- há um pequeno
+        # texto explicativo para isso.
+
+        # Ela acessa essa URL - sua lista de tarefas continua lá.
+
+        # Satisfeita, ela volta a dormir
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
